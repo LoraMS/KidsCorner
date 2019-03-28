6f504deb8d43c65fb7a06b0469b3914ff8dea47c
LoraMS <lorashopova@abv.bg>
2019-03-28 23:46:44 +0200

    slider refactoring

---
+++ scripts/pluginsBundler.js
@@ -0,0 +1,11 @@
+// Slick slider
+$(document).on('ready', function () {
+    $(".fade").slick({
+        dots: true,
+        arrows: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
+});
